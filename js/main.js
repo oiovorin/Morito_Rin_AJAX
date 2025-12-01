31b8566c7c54d32f5b49caffad18f4b389306fff
rin <rinoiod@gmail.com>
2025-12-01 18:07:21 -0500

    styled error message
    style the error message window

--- js/main.js
+++ js/main.js
@@ -5,32 +5,6 @@
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
 
-  //This information needs to be removed then pulled with an AJAX Call using the Fetch API
-  //this is the api url https://swiftpixel.com/earbud/api/materials"
-
-  const materialListData = [
-    {
-      heading: "Precision-Crafted Polymers",
-      description: "Our earbuds are meticulously molded from high-quality plastics, ensuring a blend of elegance, comfort, and resilience that's second to none."
-    },
-    {
-      heading: "Luxurious Silicone Harmony",
-      description: "Our uniquely engineered ear tips are cocooned in plush silicone, delivering an opulent embrace for your ears, ensuring an unrivaled fit and exquisite audio experience."
-    },
-    {
-      heading: "Rubberized Cables",
-      description: "Experience the unparalleled freedom of movement with our flexible rubber cables that promise durability without compromise."
-    },
-    {
-      heading: "Enhanced Comfort Sensors",
-      description: "A touch of magic in the form of built-in microphones and sensors empowers your earbuds to obey your every command, making your audio journey seamless and enchanting."
-    },
-    {
-      heading: "Artistic Mesh Guard",
-      description: "Shielded by artful mesh screens, our speakers remain untarnished, keeping your listening experience pristine."
-    }
-  ];
-
   //functions
   function loadInfoBoxes() {
 
@@ -53,8 +27,12 @@
     });
     })
     .catch(error => {
-      //make a meaningful error message and post to DOM
       console.log(error);
+      const errorText = document.createElement('p');
+      errorText.textContent = "Oops! Unable to fetch the infobox data. Please try again.";
+      errorText.classList.add("error-message")
+
+      document.body.appendChild(errorText);
     });
 
    
@@ -63,38 +41,35 @@
 
   function loadMaterialInfo() {
 
-    //Add loader in HTML, write code to show it here
       const loader = document.querySelector("#loader");
       loader.style.display = "block";
 
-    //make AJAX Call here
     fetch("https://swiftpixel.com/earbud/api/materials")    
-     //this is the api url https://swiftpixel.com/earbud/api/materials"
-
     .then(response => response.json())
     .then(materials => {
       console.log(materials);
 
       materials.forEach(material => {
-      // clone the template li with h3 and p inside
       const clone = materialTemplate.content.cloneNode(true);
-      // populate the cloned template
       const materialHeading = clone.querySelector(".material-heading");
       materialHeading.textContent = material.heading;
 
       const materialDescription = clone.querySelector(".material-description");
       materialDescription.textContent = material.description;
 
-      //Hide the loader
       loader.style.display = "none";
 
-      //Append the populated template to the list
       materialList.appendChild(clone);
     });
 
     })
     .catch(error => {
       console.log(error);
+      const errorText = document.createElement('p');
+      errorText.textContent = "Oops! Unable to fetch the material data. Please try again.";
+      errorText.classList.add("material-error-message")
+
+      document.body.appendChild(errorText);
     });
 
 
